cee8f0bb4315e8fc2fb82b9b550ccd047c30863b
kritanjan-polley <104536210+kritanjan-polley@users.noreply.github.com>
2024-03-22 15:58:55 -0700

    Update extractThermo.sh

--- shooting/extractThermo.sh
+++ shooting/extractThermo.sh
@@ -2,14 +2,11 @@
 start_time="$(date -u +%s.%N)"
 
 j=0
-for i in intermediate.shooting.*
+for i in $(seq 0 1 50)
 do 
-j=$(($j + 1))
-awk '/[[:blank:]]*Time [[:blank:]]*Econserve/{flag=1; next} /[[:blank:]]*Loop [[:blank:]]*time/{flag=0} flag' $i >  foo
-# awk '{print $1,$8}' foo >  bar ##ca.$j.out
-grep '[0-9][0-9]*' foo > $i
-rm foo
+  awk '{print $1,$8,$9}' semi.$i.txt > final.$i.txt  
 done
+
 
 end_time="$(date -u +%s.%N)"
 elapsed="$(bc <<<"$end_time-$start_time")"
