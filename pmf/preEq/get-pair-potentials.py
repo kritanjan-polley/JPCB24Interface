db5ad52cab0bf5051bd3aa43ae011194b17d7202
kritanjan-polley <104536210+kritanjan-polley@users.noreply.github.com>
2024-03-22 16:03:41 -0700

    Update get-pair-potentials.py

--- pmf/preEq/get-pair-potentials.py
+++ pmf/preEq/get-pair-potentials.py
@@ -1,7 +1,13 @@
 import ForceField as build
 
-# ff = build.ForceField(['swm4-ndp'], polarizable=True, symmetrized=True)
-# ff.print('pair-drude_tip4p.lmp')
-
+# for water + NaI + NaCl
 ff = build.ForceField(['swm4-ndp','O3','polNa','polI','polCl'], polarizable=True, symmetrized=True)
 ff.print('pair-drude_tip4p_O3_NaICl.lmp')
+
+# for pure water 
+# ff = build.ForceField(['swm4-ndp','O3','polNa'], polarizable=True, symmetrized=True)
+# ff.print('pair-drude_tip4p_O3_water.lmp')
+
+# for  water + NaI
+# ff = build.ForceField(['swm4-ndp','O3','polNa','polI'], polarizable=True, symmetrized=True)
+# ff.print('pair-drude_tip4p_O3_NaI.lmp')
